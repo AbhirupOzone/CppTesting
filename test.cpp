19bc6ee0a8988152d3a6a590aed4d51bc93dd828
AbhirupOzone <122984485+AbhirupOzone@users.noreply.github.com>
2023-03-20 14:00:42 +0530

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,18 @@
+// #include <catch2/catch.hpp>
+#include</home/ozone/Desktop/TestingCpp/catch.hpp>
+#include<bits/stdc++.h>
+using namespace std;
+
+TEST_CASE( "Adding two integers", "[math]" ) {
+    REQUIRE( 1 + 1 == 2 );
+}
+
+TEST_CASE( "Subtracting two integers", "[math]" ) {
+    REQUIRE( 1 - 1 == 0 );
+}
+
+// int main(int argc, char * argv[])
+// {
+//     cout<<"Abhirup Bhattacharya\n";
+//         return 0;
+// }
