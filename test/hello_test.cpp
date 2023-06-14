6932d5d29fb55aec1651603333ee34bd12fc7b51
AbhirupOzone <122984485+AbhirupOzone@users.noreply.github.com>
2023-06-15 00:47:07 +0530

    Create hello_test.cpp

---
+++ test/hello_test.cpp
@@ -0,0 +1,9 @@
+#define CATCH_CONFIG_MAIN
+
+TEST_CASE( "Adding two integers", "[math]" ) {
+    REQUIRE( 1 + 1 == 2 );
+}
+
+TEST_CASE( "Subtracting two integers", "[math]" ) {
+    REQUIRE( 1 + 1 == 0 );
+}
