09fdb52925a62b9f2f27fbfd907428c89f1ceca0
AbhirupOzone <122984485+AbhirupOzone@users.noreply.github.com>
2023-09-21 13:32:21 +0530

    Update Alltests_test.cpp

--- Alltests_test.cpp
+++ Alltests_test.cpp
@@ -5,7 +5,7 @@
 #include "Schooltest.cpp"
 
 TEST_CASE( "Adding two integers", "[math]" ) {
-    REQUIRE( 1 + 1 == 0 );
+    REQUIRE( 1 + 1 == 2 );
 }
 
 TEST_CASE( "Subtracting two integers", "[math]" ) {
